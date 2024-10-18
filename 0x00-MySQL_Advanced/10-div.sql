b29302a5e0ddeca2a14c9ebff149157e0b7ad5b5
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-10-18 21:36:00 +0000

    task 10:updated Safe divide

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,16 +1,15 @@
 -- This script creates a function SafeDiv that divides a by b and returns 0 if b is 0
+DROP FUNCTION IF EXISTS SafeDiv;
 DELIMITER $$
-
 CREATE FUNCTION SafeDiv(a INT, b INT)
-RETURNS DECIMAL(10, 4)
-DETERMINISTIC
+RETURNS FLOAT DETERMINISTIC
 BEGIN
-    -- Check if b is 0 to avoid division by zero
-    IF b = 0 THEN
-        RETURN 0;
+    IF (b = 0)
+    THEN
+        RETURN (0);
     ELSE
-        RETURN CAST(a AS DECIMAL(10, 4)) / CAST(b AS DECIMAL(10, 4));
+        RETURN (a / b);
     END IF;
-END $$
-
+END
+$$ 
 DELIMITER ;
